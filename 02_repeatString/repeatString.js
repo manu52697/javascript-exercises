6f11fbc845dfd09969059a4670ed7293ad403163
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2024-08-14 11:28:59 +0200

    Make first test pass for ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+	let res = '';
+	for (i = 0; i < times; i ++){
+		res += str;
+	}
+	return res;
 };
 
 // Do not edit below this line
