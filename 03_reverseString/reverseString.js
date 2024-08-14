ae0477b7df48022a22d4a0c0e8ddcd3d5fa64002
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2024-08-14 11:55:04 +0200

    Solve excercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	let res = []
+	let chars = str.split('')
 
+	while(chars.length > 0) {
+		c = chars.pop()
+		res.push(c)
+	}
+
+
+	return res.join('')
 };
 
 // Do not edit below this line
