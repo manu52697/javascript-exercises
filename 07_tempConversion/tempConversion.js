fda71cf93050fa06bbf3d6e6e8910330ce9fa2b7
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2024-08-14 13:31:26 +0200

    Past first test case for 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+	let res = (temp - 32) * (5/9);
+	return fmtResult(res)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+	let res = (x * 9 / 5) + 32;
+	return fmtResult(res);
 };
+
+function fmtResult(temp){
+	if(temp % 1 != 0) return temp.toFixed(1);
+	return temp;
+}
 
 // Do not edit below this line
 module.exports = {
